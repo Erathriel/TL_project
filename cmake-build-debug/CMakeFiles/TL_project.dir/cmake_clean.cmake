221a27a659b408aa41c9427d83a40aed8c35796e
Erathriel <geoffreynoro@orange.fr>
2017-12-06 18:02:26 +0100

    add gtest

---
+++ cmake-build-debug/CMakeFiles/TL_project.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TL_project.dir/src/fa.c.o"
+  "CMakeFiles/TL_project.dir/src/testfa.c.o"
+  "TL_project.pdb"
+  "TL_project"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TL_project.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
